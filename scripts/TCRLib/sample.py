ea927a3afe8e03c8443cf0d52fe11af0f8de05ed
elhb <erik.borgstrom@scilifelab.se>
2015-02-24 19:46:18 +0100

    now aligning reads

--- scripts/TCRLib/sample.py
+++ scripts/TCRLib/sample.py
@@ -14,6 +14,7 @@ class Sample(object):
 	self.logPath    = TCRcaller.path+'/samples/'+self.name+'/logs'
 	self.fastqcPath = TCRcaller.path+'/samples/'+self.name+'/fastQC'
 	self.dependencies = {}
+	self.TCRcaller = TCRcaller
 
     @property
     def readCount(self, ):
@@ -26,7 +27,7 @@ class Sample(object):
 
     def getFastqs(self,):
 	self.fastqIds = []
-	for filePairId,readCount,fastq1,fastq2,sampleId in AnalysisPipe.database.getFastqs():
+	for filePairId,readCount,fastq1,fastq2,sampleId in self.TCRcaller.database.getFastqs():
 	    if int(sampleId) == self.id:
 		self.fastqIds.append(filePairId)
 		yield [filePairId,readCount,fastq1,fastq2,sampleId]
@@ -45,10 +46,66 @@ class Sample(object):
         except OSError:pass
 
     def align(self,):
-	#bowtie2 -x references/ncbi.T_cell_receptor_beta.Human.mRNA.fa -U data/mnt/davidson/sandberglab/pipeline2.0/rnaseq/hsa/simone_picelli_T-cells/rawdata/$i/Run00221_L7_1_140213_SN893_0221_BC3H8JACXX_*.fastq.gz -U data/mnt/davidson/sandberglab/pipeline2.0/rnaseq/hsa/simone_picelli_T-cells/rawdata/$i/Run00223_L3_1_140220_SN893_0223_BC3JDHACXX_*.fastq.gz -S $i.sam --very-sensitive-local -p16
-	#samtools view -Sb $i.sam > $i.bam
-	#samtools sort $i.bam $i.sorted
-        #samtools index $i.sorted.bam
+
+	import glob
+	import os
+	import sys
+	import subprocess
+	import multiprocessing
+
+        # find script path
+        path = os.path.abspath(sys.argv[0])
+        path = os.path.abspath('/'.join(sys.argv[0].split('/')[:-2]))
+
+	latestref = newest = max(glob.iglob(path+'/references/ncbi.*.fa'), key=os.path.getctime)
+
+	for fastq in self.getFastqs():
+	    
+	    sys.stderr.write('aligning '+self.name+' fastq '+fastq[2]+'...')
+	    command = [path+'/bin/bowtie2-2.2.4/'+'bowtie2','-x',latestref,'-U',fastq[2],'-U',fastq[3],'-S',self.dataPath+'/'+str(fastq[0])+'.sam','--very-sensitive-local','-p',str(multiprocessing.cpu_count())]
+	    #sys.stderr.write('running: '+' '.join(command)+'\n')
+	    bt2 = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE )
+	    errdata = bt2.communicate()
+	    if bt2.returncode != 0:
+	        print 'cmd: '+' '.join( command )
+	        print 'bt2 view Error code', bt2.returncode, errdata
+	        sys.exit()
+	    sys.stderr.write('done\n')	    
+
+	    sys.stderr.write('converting '+self.name+' samfile '+str(fastq[0])+' to bam...')
+	    bam = open(self.dataPath+'/'+str(fastq[0])+'.bam','wb')
+	    command = [path+'/bin/samtools-1.2/'+'samtools','view','-Sb',self.dataPath+'/'+str(fastq[0])+'.sam']
+	    samtools = subprocess.Popen(command,stdout=bam,stderr=subprocess.PIPE )
+	    errdata = samtools.communicate()
+	    if samtools.returncode != 0:
+	        print 'cmd: '+' '.join( command )
+	        print 'bt2 view Error code', samtools.returncode, errdata
+	        sys.exit()
+	    sys.stderr.write('done\n')
+	    os.remove(self.dataPath+'/'+str(fastq[0])+'.sam')
+	    bam.close()
+
+	    sys.stderr.write('sorting '+self.name+' bamfile '+str(fastq[0])+'...')
+	    command = [path+'/bin/samtools-1.2/'+'samtools','sort',self.dataPath+'/'+str(fastq[0])+'.bam',self.dataPath+'/'+str(fastq[0])+'.sorted']
+	    samtools = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE )
+	    errdata = samtools.communicate()
+	    if samtools.returncode != 0:
+	        print 'cmd: '+' '.join( command )
+	        print 'bt2 view Error code', samtools.returncode, errdata
+	        sys.exit()
+	    sys.stderr.write('done\n')
+	    os.remove(self.dataPath+'/'+str(fastq[0])+'.bam')
+
+	    sys.stderr.write('indexing '+self.name+' bamfile '+str(fastq[0])+'...')
+	    command = [path+'/bin/samtools-1.2/'+'samtools','index',self.dataPath+'/'+str(fastq[0])+'.sorted.bam']
+	    samtools = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE )
+	    errdata = samtools.communicate()
+	    if samtools.returncode != 0:
+	        print 'cmd: '+' '.join( command )
+	        print 'bt2 view Error code', samtools.returncode, errdata
+	        sys.exit()
+	    sys.stderr.write('done\n')
+
 	pass
     
     def getAlignedReadsPairs(self, ):
